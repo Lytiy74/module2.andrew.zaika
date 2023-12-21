deecae1a6b8b6e8ffbfccc0b3357c4bf82151efc
Andrew <146869625+Lytiy74@users.noreply.github.com>
2023-12-21 19:52:52 +0200

    Update LifeCycle.java

--- src/main/java/com/andrew/service/LifeCycle.java
+++ src/main/java/com/andrew/service/LifeCycle.java
@@ -19,7 +19,6 @@ public class LifeCycle {
         executor = Executors.newFixedThreadPool(2);
     }
     public void start() throws InterruptedException {
-        logInfo();
         while (true){
             Future<?> submitAnimalCycle = executor.submit(() ->{
 
@@ -28,15 +27,6 @@ public class LifeCycle {
 
             });
             TimeUnit.MICROSECONDS.sleep(ConfigLoader.getIntProperty(CONFIG_KEY + "wait-time"));
-        }
-    }
-    private void logInfo(){
-        final GameField gameField = GameField.getInstance();
-        for (int i = 0; i < gameField.getHeight(); i++) {
-            for (int j = 0; j < gameField.getWidth(); j++) {
-                final Cell cell = gameField.getCellAt(i, j);
-                log.info("Cell " + i + " " + j);
-            }
         }
     }
 }
